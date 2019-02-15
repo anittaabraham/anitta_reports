31c40f0aff93d7a653a876e5ce3f893e440332a7
anittaabraham <43873223+anittaabraham@users.noreply.github.com>
2019-02-15 11:01:33 +0530

    Add files via upload

---
+++ country.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2019 at 08:50 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `country`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `countryID` int(10) NOT NULL AUTO_INCREMENT,
+  `country_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`countryID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`countryID`, `country_name`) VALUES
+(1, 'india'),
+(2, 'america');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `district`
+--
+
+CREATE TABLE IF NOT EXISTS `district` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `stateID` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `district`
+--
+
+INSERT INTO `district` (`id`, `stateID`, `name`) VALUES
+(1, 1, 'Kannur'),
+(2, 1, 'Kottayam'),
+(3, 2, 'uduppi'),
+(4, 2, 'shimoga'),
+(5, 3, 'manhattan'),
+(6, 3, 'bronx'),
+(7, 4, 'los angeles'),
+(8, 4, 'san jose');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE IF NOT EXISTS `state` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `countryID` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`id`, `countryID`, `name`) VALUES
+(1, 1, 'kerala'),
+(2, 1, 'karnataka'),
+(3, 2, 'new york'),
+(4, 2, 'california');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
